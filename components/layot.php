3de97e54b5366e5e185fce51c20a7a6ff0e17e5b
Gveideon <deminion0202@gmail.com>
2023-03-02 11:52:17 +0300

    add template

---
+++ components/layot.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <title><?$title ?></title>
+</head>
+
+<body>
+    <header>
+        <h1> <? $title_text ?> </h1>
+    </header>
+    <nav>
+    <ul>
+            <li>
+                <a href="index.php">Главная</a>
+            </li>
+            <li>
+                <a href="reg.php">Регистраиция</a>
+            </li>
+            <li>
+                <a href="auth.php">Авторизация</a>
+            </li>
+            <li>
+                <a href="hidden_page.php">Скрытая страница</a>
+            </li>
+        </ul>
+    </nav>
+
+    <main>
+
+    <main>
+
+    <footer>
+        <h2>
+            Суржко, 2012
+        </h2>
+    </footer>
+</body>
+
+</html>
